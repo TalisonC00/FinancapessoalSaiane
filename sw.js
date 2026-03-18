a8375050f771cdc0db0b6a4452254f5eb09d7728
TalisonC00 <srtalison00@gmail.com>
2026-03-18 00:20:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'fin-saiane-v2';
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
